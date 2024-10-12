81a745a31e9f31aad70e945ec48cfb430b1667bc
GoodwinSEV <esavvina51@gmail.com>
2024-10-12 09:38:19 +0700

    Initial Commit

---
+++ DemExam_Ushakova/DemExam_UshakovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemExam_UshakovaApp.swift
+//  DemExam_Ushakova
+//
+//  Created by User on 12.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct DemExam_UshakovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
